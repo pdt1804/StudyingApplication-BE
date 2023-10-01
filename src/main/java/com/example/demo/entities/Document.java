1838751a2bd7d714d25b69e549325485c67e648f
PHAM DUY THONG <21522645@gm.uit.edu.vn>
2023-10-01 13:38:35 +0700

    Hoàn thành table document

--- src/main/java/com/example/demo/entities/Document.java
+++ src/main/java/com/example/demo/entities/Document.java
@@ -1,5 +1,38 @@
 package com.example.demo.entities;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Document {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int documentID;
+	private String Header;
+	@Lob
+	private byte[] File;
+	private Date dateUploaded;
+	private DocumentType type;
+	@ManyToOne
+	@JoinColumn(name = "group_id")
+	private GroupStudying group;
 
 }
---
+++ src/main/java/com/example/demo/entities/DocumentType.java
@@ -0,0 +1,12 @@
+package com.example.demo.entities;
+
+public enum DocumentType {
+	
+	word,
+	excel,
+	powerpoint,
+	pdf,
+	video,
+	image
+	
+}
