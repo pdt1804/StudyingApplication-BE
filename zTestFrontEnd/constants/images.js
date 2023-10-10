31b0902bbe32e41a9c92d5d99b72891b315cc768
ADMIN <ADMIN@DESKTOP-B4C71NS>
2023-10-10 10:17:31 +0700

    Setup icon folder

--- zTestFrontEnd/constants/images.js
+++ zTestFrontEnd/constants/images.js
@@ -1,6 +1,7 @@
 export default {
   background: require('../assets/background.jpg'),
-  personIcon: require('../assets/person_icon.png'),
-  keyIcon: require('../assets/key_icon.png'),
   uitLogo: require('../assets/Logo_UIT_Web_Transparent.png'),
+  
+  personIcon: require('../assets/icon/person_icon.png'),
+  keyIcon: require('../assets/icon/key_icon.png'),
 };
